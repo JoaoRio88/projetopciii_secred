d4b4e668d9b37d20aff02d0ef4358dc4cf0dc093
JoaoRio88 <joao.rio.2024160@my.istec.pt>
2025-12-28 20:51:32 +0000

    Delete ProjetoFinal directory

--- ProjetoFinal/backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Definir as rotas de autenticação
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
